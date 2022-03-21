52ad34b169f829b1a21a7c71f6f820a686aa86d2
octogradiste <colin.berger@outlook.com>
2022-03-21 18:38:58 +0100

    Bug fix avoid rebuilding the UI in the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:can_watering/data/database.dart';
 import 'package:can_watering/domain/bloc/plant_bloc.dart';
 import 'package:can_watering/locator.dart';
-import 'package:can_watering/presentation/page/detail_page.dart';
 import 'package:can_watering/presentation/page/home_page.dart';
-import 'package:can_watering/presentation/page/plant_form_page.dart';
 import 'package:can_watering/presentation/screen/loading_screen.dart';
 import 'package:can_watering/presentation/screen/unknown_state_screen.dart';
 import 'package:can_watering/service/screen_service.dart';
@@ -40,13 +38,11 @@ class Application extends StatelessWidget {
                 return const LoadingScreen();
               } else if (state is HomeState) {
                 return HomePage(plants: state.plants);
-              } else if (state is DetailUpdateState) {
-                return DetailPage(
-                  plant: state.plant,
-                  wateringActions: state.wateringActions,
-                );
               }
               return const UnknownStateScreen();
+            },
+            buildWhen: (previous, current) {
+              return (current is LoadingState || current is HomeState);
             },
           );
         }),
